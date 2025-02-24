a2fd7445b34da6dc4bcc7c6413a85cddbc0a3c53
Irmitate <irmitate@gmail.com>
2025-02-24 17:42:42 +0000

    fhare.scss

---
+++ src/js/fshare.js
@@ -0,0 +1,22 @@
+
+
+/* Pasos en humano:
+-seleccionar el boton
+-crear un parrafo
+seleccionar un parrafo
+cuando la usuaria haga click, mostrar un mensaje
+
+-Cuando la usuaria haga click en Crear tarjeta:
+  -mostrar el párrafo: La tarjeta ha sido creada:
+      -sale un link
+      Botones con opciones de compartir en redes:
+          -mobile   - email   -instagram
+
+*/
+const buttonShare = document.querySelector(.js-btn-share)
+console.log("ahora");
+buttonShare.addEventListener("click", (event) => {
+    // EventDefault
+
+
+});
