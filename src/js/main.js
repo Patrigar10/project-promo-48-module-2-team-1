77fb342c7e955796e64ff892153859c9ac5396bc
Patrigar10 <patri.garcia.gomez@gmail.com>
2025-02-25 19:02:47 +0100

    date

--- src/js/main.js
+++ src/js/main.js
@@ -4,6 +4,7 @@ const formFill = document.querySelector(".js-formfill");
 const previewName = document.querySelector(".js-preview-name");
 const previewPhrase = document.querySelector(".js-preview-phrase");
 const previewPlace = document.querySelector(".js-preview-place");
+const previewDate = document.querySelector(".js-preview-date");
 
 formFill.addEventListener("input", (event) => {
     console.log(event.target.value);
@@ -13,6 +14,8 @@ formFill.addEventListener("input", (event) => {
         previewPhrase.innerHTML = event.target.value;
     } else if (event.target.id === "place") {
         previewPlace.innerHTML = event.target.value;
+    } else if (event.target.id === "date") {
+        previewDate.innerHTML = event.target.value;
     }
 
 })
