bf3a641a90c98a8f7ce6b99cba7977bd81686a95
Irmitate <irmitate@gmail.com>
2025-02-25 17:55:26 +0000

    js-preview

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,7 @@
 const formFill = document.querySelector(".js-formfill");
 const previewName = document.querySelector(".js-preview-name");
 const previewPhrase = document.querySelector(".js-preview-phrase");
+const previewPlace = document.querySelector(".js-preview-place");
 
 formFill.addEventListener("input", (event) => {
     console.log(event.target.value);
@@ -10,7 +11,10 @@ formFill.addEventListener("input", (event) => {
         previewName.innerHTML = event.target.value;
     } else if (event.target.id === "phrase") {
         previewPhrase.innerHTML = event.target.value;
+    } else if (event.target.id === "place") {
+        previewPlace.innerHTML = event.target.value;
     }
+
 })
 
 // import "./fshare";
