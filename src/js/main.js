20da16225702f772a984e02182da39694b1e0952
BugmaoLtd <begona.lopezperez@gmail.com>
2025-02-25 18:35:53 +0100

    Preview en formulario

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,16 @@
 'use strict';
 
-console.log('>> Ready :)');
+const formFill = document.querySelector(".js-formfill");
+const previewName = document.querySelector(".js-preview-name");
+const previewPhrase = document.querySelector(".js-preview-phrase");
 
-import "./fshare";
+formFill.addEventListener("input", (event) => {
+    console.log(event.target.value);
+    if (event.target.id === "name") {
+        previewName.innerHTML = event.target.value;
+    } else if (event.target.id === "phrase") {
+        previewPhrase.innerHTML = event.target.value;
+    }
+})
+
+// import "./fshare";
