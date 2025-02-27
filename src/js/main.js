98081109b6b912df1a3f56858886475b4a97aab3
Loreto94 <loreto0105@gmail.com>
2025-02-27 12:31:41 +0100

    Object

--- src/js/main.js
+++ src/js/main.js
@@ -6,19 +6,36 @@ const previewPhrase = document.querySelector(".js-preview-phrase");
 const previewPlace = document.querySelector(".js-preview-place");
 const previewDate = document.querySelector(".js-preview-date");
 const previewAge = document.querySelector(".js-preview-age");
+
+const formData = {
+    field1: "",
+    field2: "",
+    field3: "",
+    field4: "",
+    field5: 0,
+    field6: "",
+    
+}
+
 formFill.addEventListener("input", (event) => {
     console.log(event.target.value);
     if (event.target.id === "name") {
         previewName.innerHTML = event.target.value || "Nombre";
+        formData.field1 = event.target.value;
     } else if (event.target.id === "phrase") {
         previewPhrase.innerHTML = event.target.value || "Frase";
+        formData.field2 = event.target.value;
     } else if (event.target.id === "place") {
         previewPlace.innerHTML = event.target.value || "Lugar";
+        formData.field3 = event.target.value;
     } else if (event.target.id === "date") {
         previewDate.innerHTML = event.target.value || "Fecha";
+        formData.field4 = event.target.value;
     } else if (event.target.id === "age") {
         previewAge.innerHTML = event.target.value || "Edad";
-    } 
+        formData.field5 = event.target.value;
+    }
+    console.log(formData);
 })
 
 /* Si los campos no se rellenan deja lo que había al principio */
@@ -39,3 +56,10 @@ selectedBtn.addEventListener("click", (event) => {
     selectedForm.classList.toggle(".collpased");
     
 });
+
+const tittleArrow = document.querySelector(".js-tittle-arrow");
+const designModels = document.querySelector(".js-design-models");
+
+tittleArrow.addEventListener("click", (event) => {
+    designModels.classList.toggle(".collapsed");
+})
