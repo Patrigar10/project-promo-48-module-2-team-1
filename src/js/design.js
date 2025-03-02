bb2e511ebf7a98c64d2fbaa7e71fa6a02e167a1f
Patrigar10 <patri.garcia.gomez@gmail.com>
2025-03-02 20:11:30 +0100

    width 100% image preview

--- src/js/design.js
+++ src/js/design.js
@@ -1,10 +1,6 @@
 "use strict";
 
-
-
 // const designModels = document.querySelector(".js-design-models");
-
-
 
 // designModels.addEventListener("click", (event) => {
 //     console.log(event.target);
@@ -15,21 +11,19 @@
 
 // })
 
-
 const radio1 = document.querySelector(".js-model1");
 const radio2 = document.querySelector(".js-model2");
 const radio3 = document.querySelector(".js-model3");
-const previewCard = document.querySelector(".js-previewCard")
+const previewCard = document.querySelector(".js-previewCard");
 
 radio1.addEventListener("change", () => {
-    previewCard.classList.add("background__model1");
-})
+  previewCard.classList.add("background__model1");
+});
 
 radio2.addEventListener("change", () => {
-    previewCard.classList.add("background__model2");
-})
+  previewCard.classList.add("background__model2");
+});
 
 radio3.addEventListener("change", () => {
-    previewCard.classList.add("background__model3");
-})
-
+  previewCard.classList.add("background__model3");
+});
