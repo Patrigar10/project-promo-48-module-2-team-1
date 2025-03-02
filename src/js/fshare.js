7a643d5fbfe7c6be1a91cbe05d4b755063b22bf8
Irmitate <irmitate@gmail.com>
2025-03-02 20:12:11 +0000

    mejorando css y detallitos

--- src/js/fshare.js
+++ src/js/fshare.js
@@ -19,7 +19,7 @@ shareDisplay.addEventListener("click", handleClickThree);
 
 
 /* Pasos para crear el link de la invitación:
--Cuando la usuaria haga click en Crear invitación:
+-Cuando la usuaria haga click en el botón: Crear invitación:
   -mostrar el párrafo: 
            La tarjeta ha sido creada:
               -sale un link
@@ -28,9 +28,17 @@ shareDisplay.addEventListener("click", handleClickThree);
 
           */
 
-const createCard = document.querySelector(".js-share-btn-createCard")
-    createCard.addEventListener("click", (event) => {
+const buttonCreateCard = document.querySelector(".js-share-btn-createCard");
+const createdCardText = document.querySelector(".js-createdCardText");
+
+
+function handleClickFour (event) {
     event.preventDefault();
-    // const createCardFinal = event.target.innerText;
-    // createCardFinal.innerHTML = "La tarjeta ha sido creada:";
- });
+    createdCardText.classList.toggle("collapsed");    
+};
+buttonCreateCard.addEventListener("click", handleClickFour);
+
+
+
+
+
