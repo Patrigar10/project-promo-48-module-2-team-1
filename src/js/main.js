b84153c271d794c633c475c1cb83303d97bdf3eb
BugmaoLtd <begona.lopezperez@gmail.com>
2025-03-04 17:53:04 +0100

    Arreglillos

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,8 @@
 
 import "./design.js";
 import "./fshare.js";
+import "./fillout.js";
+import "./reset.js";
 import "./get-avatar.js";
 
 const formFill = document.querySelector(".js-formfill");
@@ -40,27 +42,6 @@ formFill.addEventListener("input", (event) => {
     formData.field5 = event.target.value;
   }
   console.log(formData);
-});
-
-/* Si los campos no se rellenan deja lo que había al principio */
-
-const resetBtn = document.querySelector(".js-reset-btn");
-// const previewList = document.querySelector(".js-preview-list");
-
-resetBtn.addEventListener("click", (event) => {
-  event.preventDefault();
-  formFill.reset();
-  previewName.innerHTML = "Nombre";
-  previewPhrase.innerHTML = "Frase";
-  previewPlace.innerHTML = "Lugar";
-  previewDate.innerHTML = "Fecha";
-  previewAge.innerHTML = "Edad";
-  previewCard.classList.remove("background__model1");
-  previewCard.classList.remove("background__model2");
-  previewCard.classList.remove("background__model3");
-  document.querySelector(".js__profile-image").src = "./Images/nina.png";
-  document.querySelector(".js__profile-preview").src = "./Images/nina.png";
-  document.querySelector(".js__profile-upload-btn").value = "";
 });
 
 // Formulario fillout colapsed
