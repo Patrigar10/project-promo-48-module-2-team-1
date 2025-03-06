354bf9cd11253e44e8b2e64b6af698b0873ec562
Patrigar10 <patri.garcia.gomez@gmail.com>
2025-03-06 13:46:37 +0100

    collapsable part II and remove buttons

--- src/js/collapsable.js
+++ src/js/collapsable.js
@@ -1,32 +1,57 @@
-// Formulario fillout colapsed
+//FUNCIÓN COLLAPSABLES NUEVA (ESCUCHANDO EL CLICK EN LEGEND)
 
-const tittleArrow = document.querySelector(".js-design-arrow");
 const designModels = document.querySelector(".js-design-models");
-const selectedBtn = document.querySelector(".js-btn-add");
 const filloutFormDisplay = document.querySelector(".js-fillout-display");
-const shareDisplay = document.querySelector(".js-btn-shareDisplay");
 const shareOptions = document.querySelector(".js-shareOptions");
 
-function handleClickTwo(event) {
-  event.preventDefault();
-  designModels.classList.remove("collapsed");
-  filloutFormDisplay.classList.add("collapsed");
-  shareOptions.classList.add("collapsed");
-}
-tittleArrow.addEventListener("click", handleClickTwo);
+const collapsableHeaders = document.querySelectorAll(".js_collapsable_header");
 
-function handleClick(event) {
-  event.preventDefault();
-  filloutFormDisplay.classList.remove("collapsed");
-  designModels.classList.add("collapsed");
-  shareOptions.classList.add("collapsed");
-}
-selectedBtn.addEventListener("click", handleClick);
+const handleClick = (event) => {
+  if (event.currentTarget.classList.contains("js_legend_design")) {
+    designModels.classList.remove("collapsed");
+    filloutFormDisplay.classList.add("collapsed");
+    shareOptions.classList.add("collapsed");
+  } else if (event.currentTarget.classList.contains("js_legend_fillout")) {
+    filloutFormDisplay.classList.remove("collapsed");
+    designModels.classList.add("collapsed");
+    shareOptions.classList.add("collapsed");
+  } else {
+    shareOptions.classList.remove("collapsed");
+    filloutFormDisplay.classList.add("collapsed");
+    designModels.classList.add("collapsed");
+  }
+};
 
-function handleClickThree(event) {
-  event.preventDefault();
-  shareOptions.classList.remove("collapsed");
-  filloutFormDisplay.classList.add("collapsed");
-  designModels.classList.add("collapsed");
+for (const legend of collapsableHeaders) {
+  legend.addEventListener("click", handleClick);
 }
-shareDisplay.addEventListener("click", handleClickThree);
+
+//FUNCIONES COLAPSABLES Y CONSTANTES ANTIGUAS
+
+// const tittleArrow = document.querySelector(".js-design-arrow");
+// const selectedBtn = document.querySelector(".js-btn-add");
+// const shareDisplay = document.querySelector(".js-btn-shareDisplay");
+
+// function handleClickTwo(event) {
+//   event.preventDefault();
+//   designModels.classList.remove("collapsed");
+//   filloutFormDisplay.classList.add("collapsed");
+//   shareOptions.classList.add("collapsed");
+// }
+// tittleArrow.addEventListener("click", handleClickTwo);
+
+// function handleClick(event) {
+//   event.preventDefault();
+//   filloutFormDisplay.classList.remove("collapsed");
+//   designModels.classList.add("collapsed");
+//   shareOptions.classList.add("collapsed");
+// }
+// selectedBtn.addEventListener("click", handleClick);
+
+// function handleClickThree(event) {
+//   event.preventDefault();
+//   shareOptions.classList.remove("collapsed");
+//   filloutFormDisplay.classList.add("collapsed");
+//   designModels.classList.add("collapsed");
+// }
+// shareDisplay.addEventListener("click", handleClickThree);
